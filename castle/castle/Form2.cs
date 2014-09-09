20bc90dd9b5fadbd198fdfe161742b207478aa49
giokats <giorgikatsis@gmail.com>
2014-09-09 04:16:27 -0700

    to kastraki mas

---
+++ castle/castle/Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Castle
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3 myform = new Form3();
+            this.Hide();
+            myform.ShowDialog();
+            this.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form4 myform = new Form4();
+            this.Hide();
+            myform.ShowDialog();
+            this.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form5 myform = new Form5();
+            this.Hide();
+            myform.ShowDialog();
+            this.Show();
+        }
+    }
+}
